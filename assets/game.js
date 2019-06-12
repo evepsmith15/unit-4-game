a20ad0bf5ca1b0fcb3f76b9c0a9914fc8b464387
evepsmith15 <50637882+evepsmith15@users.noreply.github.com>
2019-06-11 20:05:52 -0700

    Add files via upload

---
+++ assets/game.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+    //variable list
+    var desultory = 0;
+    var win = 0;
+    var losses = 0;
+    var total = 0; //user's score
+    var CPUNumber; //number to match 
+    var aud = null;
+    var emeralds = { //instead of having 4 different variables, all respond to one 
+        red: {
+            name: "Red",
+            value: 0
+        },
+        blue: {
+            name: "Blue",
+            value: 0
+        },
+        yellow: {
+            name: "Yellow",
+            value: 0
+        },
+        green: {
+            name: "Green",
+            value: 0
+        },
+    };
+
+    //----------------------------------------------
+
+    //code to randomly generate a number
+    CPUNumber = function (min, max) { //takes a minium and maximum number
+        return Math.floor(Math.random() * (max - min + 1)) + min; //prevents the number from being 0
+    };
+
+    //target score 
+    desultory = function (min, max) { //takes a minium and maximum number
+        return Math.floor(Math.random() * (max - min + 1)) + min; //prevents the number from being 0
+    };
+
+    var playAudioWin = new Audio('./assets/Sonic 3 Music Ice Cap Zone Act 1.mp3');
+    //var w = document.getElementById("SoundClipsWin");
+    //var l = document.getElementById("SoundClipLosses");
+    playAudioWin.play();
+
+    var playAudioLose = new Audio('./assets/tiduss-laugh.mp3');
+    playAudioLose.play();
+
+    //starts the game
+    var Start = function () {
+        total = 0;
+        desultory = CPUNumber(30, 150); //range for the main score
+        emeralds.red.value = CPUNumber(1, 10);
+        emeralds.blue.value = CPUNumber(1, 10);
+        emeralds.yellow.value = CPUNumber(1, 10);
+        emeralds.green.value = CPUNumber(1, 10);
+        $("#total").text(total);
+        $("#Emerald Value").text(desultory);
+        console.log("Emerald Value: " + desultory);
+    };
+
+    //code to give the crystals a random number
+    var addValue = function (clickEmerald) {
+        total += clickEmerald.value;
+        $("#total").text(total);
+        WinLose();
+        console.log("Score: " + total);
+    };
+
+
+    //win/lose condition
+    var WinLose = function () {
+
+        if (total === desultory) {
+            win++;
+            console.log(total);
+            $("#total").text(total);
+            $("#win").text(win);
+            playAudioWin();
+            Start();
+        }
+        else if (total > desultory) {
+            losses++;
+            console.log(total);
+            $("#total").text(total);
+            $("#losses").text(losses);
+            playAudioLose();
+            Start();
+        }
+    };
+    Start();
+    $("#buttonRed").on("click", function () {
+        addValue(emeralds.red);
+    });
+    $("#buttonBlue").on("click", function () {
+        addValue(emeralds.blue);
+    });
+    $("#buttonYellow").on("click", function () {
+        addValue(emeralds.yellow);
+    });
+    $("#buttonGreen").on("click", function () {
+        addValue(emeralds.green);
+    });
+
+});
