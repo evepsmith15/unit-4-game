f21422acacc233d396d42b725d33330868070d7f
alexharrisonmusic <alexanderjosephharrison@gmail.com>
2019-06-11 20:16:55 -0700

    alex restructured for github

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
     //variable list
-    var desultory;
+    var desultory = 0;
     var win = 0;
     var losses = 0;
     var total = 0; //user's score
     var CPUNumber; //number to match 
+    var aud = null;
     var emeralds = { //instead of having 4 different variables, all respond to one 
         red: {
             name: "Red",
@@ -37,20 +38,25 @@ $(document).ready(function () {
         return Math.floor(Math.random() * (max - min + 1)) + min; //prevents the number from being 0
     };
 
-    playAudio = function() {
+    var playAudioWin = new Audio('./assets/Sonic 3 Music Ice Cap Zone Act 1.mp3');
+    //var w = document.getElementById("SoundClipsWin");
+    //var l = document.getElementById("SoundClipLosses");
+    playAudioWin.play();
 
-    };
+    var playAudioLose = new Audio('./assets/tiduss-laugh.mp3');
+    playAudioLose.play();
 
     //starts the game
     var Start = function () {
         total = 0;
-        desultory = (30, 150); //range for the main score
+        desultory = CPUNumber(30, 150); //range for the main score
         emeralds.red.value = CPUNumber(1, 10);
         emeralds.blue.value = CPUNumber(1, 10);
         emeralds.yellow.value = CPUNumber(1, 10);
         emeralds.green.value = CPUNumber(1, 10);
         $("#total").text(total);
-        $("#Emerald Value").text(CPUNumber);
+        $("#Emerald Value").text(desultory);
+        console.log("Emerald Value: " + desultory);
     };
 
     //code to give the crystals a random number
@@ -64,41 +70,36 @@ $(document).ready(function () {
 
     //win/lose condition
     var WinLose = function () {
-        var w = document.getElementById(SoundClipsWin);
-        var l = document.getElementById(SoundClipLosses);
-        if (total === CPUNumber) {
+
+        if (total === desultory) {
             win++;
             console.log(total);
             $("#total").text(total);
-            $("#wins").text("Wins: " + win);
-            function playAudio() {
-                w.play();
-            }
+            $("#win").text(win);
+            playAudioWin();
             Start();
         }
-        else if (total > CPUNumber) {
+        else if (total > desultory) {
             losses++;
             console.log(total);
             $("#total").text(total);
-            $("#losses").text("Losses: " + losses);
-            function playAudio() {
-                l.play();
-            }
+            $("#losses").text(losses);
+            playAudioLose();
             Start();
         }
     };
     Start();
     $("#buttonRed").on("click", function () {
-        addValue(Emerald.red);
+        addValue(emeralds.red);
     });
     $("#buttonBlue").on("click", function () {
-        addValue(Emerald.blue);
+        addValue(emeralds.blue);
     });
     $("#buttonYellow").on("click", function () {
-        addValue(Emerald.yellow);
+        addValue(emeralds.yellow);
     });
     $("#buttonGreen").on("click", function () {
-        addValue(Emerald.green);
+        addValue(emeralds.green);
     });
 
 });
